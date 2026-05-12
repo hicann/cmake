f75bf3edbadb0113610f907edaa58c7555bd7cff
zhuhaoran5 <zhuhaoran5@huawei.com>
2026-05-12 11:23:17 +0800

    【chore】支持Q2工程优化
    Co-authored-by: zhuhaoran5<zhuhaoran5@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !57 merge support_Q2_building_optimization into master
    
    【chore】支持Q2工程优化
    
    Created-by: zhuhaoran5
    Commit-by: zhuhaoran5
    Merged-by: cann-robot
    Description: 
    ## 变更描述 / Description
    Modify 'third_party/symengine.cmake'.
    
    ## 改动类型 / Change Type
    
    - [ ] Bug 修复 / Bug Fix
    - [ ] 新功能 / New Feature
    - [ ] 性能优化 / Performance
    - [x] 代码重构 / Refactoring
    - [ ] 文档更新 / Documentation
    - [ ] 测试相关 / Test
    - [ ] 其它 / Other
    
    ## 关联 Issue / Related Issues
    
    <!-- Closes #000 可自动关闭 / Closes #000 to auto-close -->
    
    - Closes #
    - References #
    
    ## 测试信息 / Testing
    
    <!-- 简要测试说明或关键结果 / Brief test description or key results -->
    
    - [x] 单元测试通过 / UT passed
    - [ ] 集成测试通过 / ST passed
    - [x] 人工验证通过 / Manual verified
    
    ## 检查清单 / Checklist
    
    - [x] 代码符合规范 / Code follows style guide
    - [ ] 测试添加并通过 / Tests added and passed
    - [ ] 文档已更新 / Docs updated if needed
    - [ ] 无硬编码敏感信息 / No secrets hardcoded
    - [x] 提交信息符合规范 / Commit message follows convention
    
    See merge request: cann/cmake!57

--- third_party/symengine.cmake
+++ third_party/symengine.cmake
@@ -12,12 +12,21 @@ if (TARGET symengine_build)
     return()
 endif ()
 
+add_library(symengine_static STATIC IMPORTED)
+if(NOT EXISTS ${CANN_3RD_LIB_PATH}/symengine/include)
+    file(MAKE_DIRECTORY "${CANN_3RD_LIB_PATH}/symengine/include")
+endif()
+set_target_properties(symengine_static PROPERTIES
+    IMPORTED_LOCATION "${CANN_3RD_LIB_PATH}/symengine/lib/libsymengine.a"
+    INTERFACE_INCLUDE_DIRECTORIES "${CANN_3RD_LIB_PATH}/symengine/include"
+)
+
 include(ExternalProject)
 
-set(LIB_FILE "${CMAKE_THIRD_PARTY_LIB_DIR}/symengine/lib") # 编译之后才会有的文件，用于判断是否已经编译
-set(MOD_FILE "${CMAKE_THIRD_PARTY_LIB_DIR}/symengine/symengine/mod.cpp") # 打上patch之后才会有的文件,用于判断是否打了patch
-set(CMAKE_FILE "${CMAKE_THIRD_PARTY_LIB_DIR}/symengine/CMakeLists.txt") # 用于判断是否已下载并解压
-set(REQ_URL "${CMAKE_THIRD_PARTY_LIB_DIR}/symengine/symengine-0.12.0.tar.gz")
+set(LIB_FILE "${CANN_3RD_LIB_PATH}/symengine/lib") # 编译之后才会有的文件，用于判断是否已经编译
+set(MOD_FILE "${CANN_3RD_LIB_PATH}/symengine/symengine/mod.cpp") # 打上patch之后才会有的文件,用于判断是否打了patch
+set(CMAKE_FILE "${CANN_3RD_LIB_PATH}/symengine/CMakeLists.txt") # 用于判断是否已下载并解压
+set(REQ_URL "${CANN_3RD_LIB_PATH}/symengine/symengine-0.12.0.tar.gz")
 set(SYMENGINE_EXTRA_ARGS "")
 if(EXISTS ${LIB_FILE})
     message(STATUS "[ThirdParty][symengine] ${LIB_FILE} found, symengine is ready after compile.")
@@ -27,51 +36,46 @@ else()
     elseif(EXISTS ${CMAKE_FILE})
         message(STATUS "[ThirdParty][symengine] ${CMAKE_FILE} found, symengine is ready without patch installed.")
         list(APPEND SYMENGINE_EXTRA_ARGS
-            PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_LIST_DIR}/patch/symengine_add_mod.patch
+            PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_LIST_DIR}/symengine_add_mod.patch
         )
     elseif(EXISTS ${REQ_URL})
         message(STATUS "[ThirdParty][symengine] ${REQ_URL} found.")
         list(APPEND SYMENGINE_EXTRA_ARGS
             URL ${REQ_URL}
-            PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_LIST_DIR}/patch/symengine_add_mod.patch
+            PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_LIST_DIR}/symengine_add_mod.patch
         )
     else()
         message(STATUS "[ThirdParty][symengine] symengine not found, need download.")
         set(REQ_URL "https://gitcode.com/cann-src-third-party/symengine/releases/download/v0.12.0/symengine-0.12.0.tar.gz")
         list(APPEND SYMENGINE_EXTRA_ARGS
             URL ${REQ_URL}
-            DOWNLOAD_DIR ${CMAKE_THIRD_PARTY_LIB_DIR}/symengine
-            PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_LIST_DIR}/patch/symengine_add_mod.patch
+            DOWNLOAD_DIR ${CANN_3RD_LIB_PATH}/symengine
+            PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_LIST_DIR}/symengine_add_mod.patch
         )
     endif()
-    set(SYMENGINE_CXXFLAGS "-fPIC -D_GLIBCXX_USE_CXX11_ABI=${USE_CXX11_ABI} -std=c++17")
-
+    set(SYMENGINE_CXXFLAGS "-fPIC -D_GLIBCXX_USE_CXX11_ABI=0 -std=c++17")
     ExternalProject_Add(symengine_build
-            SOURCE_DIR ${CMAKE_THIRD_PARTY_LIB_DIR}/symengine
+            SOURCE_DIR ${CANN_3RD_LIB_PATH}/symengine
             ${SYMENGINE_EXTRA_ARGS}
             TLS_VERIFY OFF
             CONFIGURE_COMMAND ${CMAKE_COMMAND}
                 -DINTEGER_CLASS:STRING=boostmp
                 -DBUILD_SHARED_LIBS:BOOL=OFF
-                -DBOOST_ROOT=${CMAKE_THIRD_PARTY_LIB_DIR}/boost
+                -DBOOST_ROOT=${CANN_3RD_LIB_PATH}/boost-1.87.0
                 -DBUILD_TESTS=off
                 -DCMAKE_POLICY_VERSION_MINIMUM=3.5
                 -DCMAKE_CXX_STANDARD=17
                 -DWITH_SYMENGINE_THREAD_SAFE:BOOL=ON
                 -DCMAKE_CXX_EXTENSIONS=OFF
                 -DCMAKE_CXX_FLAGS=${SYMENGINE_CXXFLAGS}
-                -DCMAKE_C_COMPILER_LAUNCHER=${CMAKE_C_COMPILER_LAUNCHER}
-                -DCMAKE_CXX_COMPILER_LAUNCHER=${CMAKE_CXX_COMPILER_LAUNCHER}
-                -DCMAKE_INSTALL_PREFIX=${CMAKE_THIRD_PARTY_LIB_DIR}/symengine
-                -DCMAKE_PREFIX_PATH=${CMAKE_THIRD_PARTY_LIB_DIR}/boost
-                -DLLVM_PATH=${LLVM_PATH}
-                -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+                -DCMAKE_INSTALL_PREFIX=${CANN_3RD_LIB_PATH}/symengine
+                -DCMAKE_PREFIX_PATH=${CANN_3RD_LIB_PATH}/boost-1.87.0
                 <SOURCE_DIR>
             BUILD_COMMAND $(MAKE)
             INSTALL_COMMAND $(MAKE) install
             EXCLUDE_FROM_ALL TRUE
             )
-    if(NOT BOOST_FOUND)
-        add_dependencies(symengine_build boost_build)
-    endif()
+    include(${CMAKE_CURRENT_LIST_DIR}/boost.cmake)
+    add_dependencies(symengine_build third_party_boost)
+    add_dependencies(symengine_static symengine_build)
 endif()
