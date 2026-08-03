235e7104784647288a22127802b8348096b96493
gcw_UQE6G7Ro <zhoudaijin@huawei.com>
2026-08-03 14:40:31 +0800

    back the host protoc copy logic, and fix the math's protobuf src path
    Co-authored-by: gcw_UQE6G7Ro<zhoudaijin@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !246 merge master into master
    
    back the host protoc copy logic, and fix the math's protobuf src path
    
    Created-by: gcw_UQE6G7Ro
    Commit-by: gcw_UQE6G7Ro
    Merged-by: cann-robot
    Description: 
    ## 变更描述 / Description
    
    <!-- 本 PR 做了什么，为什么需要 / What does this PR do and why -->
    back the host protoc copy logic, and fix the math's protobuf src path
    
    ## 改动类型 / Change Type
    
    - [x] Bug 修复 / Bug Fix
    - [ ] 新功能 / New Feature
    - [ ] 性能优化 / Performance
    - [ ] 代码重构 / Refactoring
    - [ ] 文档更新 / Documentation
    - [ ] 测试相关 / Test
    - [ ] 其它 / Other
    
    
    
    See merge request: cann/cmake!246

--- third_party/protobuf.cmake
+++ third_party/protobuf.cmake
@@ -252,15 +252,11 @@ else()
         BUILD_COMMAND $(MAKE)
         INSTALL_COMMAND ${CMAKE_COMMAND} -E make_directory ${PROTOBUF_HOST_PROTOC_DIR}
             COMMAND cp <BINARY_DIR>/protoc ${PROTOBUF_HOST_PROTOC_DIR}/protoc
-            COMMAND cp -r <SOURCE_DIR>/src ${PROTOBUF_HOST_PROTOC_DIR}
         EXCLUDE_FROM_ALL TRUE
     )
     set_target_properties(host_protoc PROPERTIES IMPORTED_LOCATION ${PROTOBUF_HOST_PROTOC_DIR}/protoc)
     add_dependencies(host_protoc protobuf_host_build)
 endif()
-# use for math
-set(HOST_PROTOC_SRC ${PROTOBUF_HOST_PROTOC_DIR}/src)
-set(HOST_PROTOC_PATH ${PROTOBUF_HOST_PROTOC_DIR})
 # used for ge
 add_custom_target(protoc DEPENDS host_protoc)
 
@@ -268,7 +264,7 @@ add_custom_target(protoc DEPENDS host_protoc)
 # Target: ascend_protobuf_static
 # ---------------------------------------------------------
 add_library(ascend_protobuf_static STATIC IMPORTED GLOBAL)
-add_library(ascend_protobuf_static_headers INTERFACE IMPORTED)
+add_library(ascend_protobuf_static_headers INTERFACE IMPORTED GLOBAL)
 if(ASCEND_PROTOBUF_STATIC_INCLUDE AND ASCEND_PROTOBUF_STATIC_LIB)
     message("[ThirdParty][protobuf] protobuf_static use cache.")
     set_target_properties(ascend_protobuf_static PROPERTIES
